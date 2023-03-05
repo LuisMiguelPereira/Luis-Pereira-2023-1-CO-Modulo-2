430ce9ccdb2bb7a004a88bae6e6d242068cd85be
LuisMiguelPereira <luis.pereira@fundacion-jala.org>
2023-03-05 17:58:34 -0500

    second-class

---
+++ dino_runner/components/obstacle.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import SCREEN_WIDTH
+
+class Obstacle(Sprite):
+
+    def __init__(self, image, obstacle_type):
+        self.image = image
+        self.obstacle_type = obstacle_type
+        self.rect = self.image[obstacle_type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update(self, game_speed):
+        self.game_speed -= game_speed
+        
+    def draw(self, screen):
+        screen.blit(self.image[self.obstacle_type], (self.rect.x, self.rect.y))
+        
